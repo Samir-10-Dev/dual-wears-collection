8e5c56d45bc77f196f4d1e18d43d1ebc3e6dd0ff
sammyblythe10 <karkidholisameer@gmail.com>
2026-08-18 12:54:02 +0545

    Fix scroll position resetting to top on product navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import { Routes, Route, useLocation } from 'react-router-dom'
-import { AnimatePresence } from 'framer-motion'
-import Navbar from './components/Navbar.jsx'
-import Footer from './components/Footer.jsx'
-import Home from './pages/Home.jsx'
-import ProductDetails from './pages/ProductDetails.jsx'
+import { Routes, Route, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
+import Home from "./pages/Home.jsx";
+import ProductDetails from "./pages/ProductDetails.jsx";
+import ScrollToTop from "./components/ScrollToTop.jsx";
 
 export default function App() {
-  const location = useLocation()
+  const location = useLocation();
 
   return (
     <>
       <Navbar />
+      <ScrollToTop />
       <AnimatePresence mode="wait">
         <Routes location={location} key={location.pathname}>
           <Route path="/" element={<Home />} />
@@ -19,5 +21,5 @@ export default function App() {
       </AnimatePresence>
       <Footer />
     </>
-  )
+  );
 }
